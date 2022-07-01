41a5d2f52b00b61c2dfb622752dcdb705d335c04
michael5565 <michaelnwokobia60@gmail.com>
2022-06-30 19:01:46 -0700

    kj

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 
 {
 
-	    puts("Programming is like building a multilingual puzzle");
+	    puts("/"Programming is like building a multilingual puzzle");
 
 	        getchar();
 
