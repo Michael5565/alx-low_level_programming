9f6b89e3f0c1b775db1273b9b5f22fc3c1eaea7d
michael5565 <michaelnwokobia60@gmail.com>
2022-06-30 19:14:13 -0700

    ghj

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+/* C program to show the use of puts*/
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+	    puts("/"Programming is like building a multilingual puzzle");
+
+	        getchar();
+
+		    return (0);
+
+}
+
