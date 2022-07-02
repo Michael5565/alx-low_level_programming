a54d7f5c639c3cd5a212234da48221542d148110
michael5565 <michaelnwokobia60@gmail.com>
2022-07-02 09:56:32 -0700

    ddd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/* C program to show the use of prinf*/
+
+#include <stdio.h>
+
+/*
+ *
+ * prints the output of the program
+ *
+ * */int main(void)
+
+{
+
+	    printf("with proper grammar, but the outcome is a piece of art")
+
+		    return (0);
+
+}
