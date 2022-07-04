487053ace4c6b6dcf724765998f9e2217480952a
michael5565 <michaelnwokobia60@gmail.com>
2022-07-03 18:40:22 -0700

    fdd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,7 @@
 #include<stdlib.h>
 #include<stddef.h>
 #include<time.h>
-/* 
-* betty style doc for function main goes there 
+/* betty style doc for function main goes there 
 */int main(void)
 
 {
