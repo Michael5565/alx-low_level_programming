aa833d72d377f46af9f19ef3b30d42fc5d671c6f
michael5565 <michaelnwokobia60@gmail.com>
2022-07-04 21:21:47 -0700

    dfds

---
+++ 0x01-variables_if_else_while/5-print_nubers.c
@@ -0,0 +1,35 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - starting my program
+ *
+ *   * int ch - introduce ch
+ *
+ *    * for = for every ch <= print body
+ *
+ *     * 
+ *
+ *      * Return: always 0
+ *
+ *       */
+
+int main(void)
+
+{
+
+	    int i;
+
+	        for (i = 0; i < 10; i++)
+
+			    {
+
+				    	putchar (ch);
+
+					    }
+
+		    putchar ('\n')
+
+			        return (0);
+
+}
