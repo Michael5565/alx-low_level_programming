0b3018de6f5e4b7451b49cdad2b9bff71cd83263
michael5565 <michaelnwokobia60@gmail.com>
2022-07-04 20:56:25 -0700

    fd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include<stdio.h>                                                                                                                             
+
+/*                                                                               * print all letter all alphabet in lower case*/int main(void)
+{                                                                                                                                             
+	int ch;                                                                	                                                                                	for (ch = 'a'; ch <= 'z'; ch++)                                                                                                       
+	{
+		putchar (ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar (ch);
+	}	                                                                	putchar ('\n');                                                         	return (0);
+}                                                                              
