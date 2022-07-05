07d3c9d81718da2412ee92de979253363cfba868
michael5565 <michaelnwokobia60@gmail.com>
2022-07-04 21:45:49 -0700

    ffd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,12 +2,13 @@
 /*
 *print all letter all alphabet in lower case*/int main(void)
 {                                                                                 
-	int ch;                                                                	                                                                                	for (ch = 'a'; ch <= 'z'; ch++)                                                                                                       
+	int ch; 
+		for (ch = 'a'; ch <= 'z'; ch++)                                                                                                       
 	{
 		putchar (ch);
 	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar (ch);
-	}	                                                                	putchar ('\n');                                                         	return (0);
+		putchar ('ch');
+	putchar ('\n')
+	return (0)
 }
