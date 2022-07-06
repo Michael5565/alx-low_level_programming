dc9f00ea9d12c8a00d17834d2ca22f9eb160b69e
michael5565 <michaelnwokobia60@gmail.com>
2022-07-06 08:09:02 -0700

    gffd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,14 +4,31 @@
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <=9; i++)
 	{
-		for (j = 0; j <= 0; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= 0; j++)
 		{
-			_putchar(i * j);
-			_putchar(',');
-			_putchar(' ');
+			k = i * j;
+			if ((k / 10) > 0)
+			{
+				_putchar((k / 10) + '0');
+			}
+			else
+				_putchar(' ');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
 		}
+	}
+		_putchar('\n');
 	}
 }
