61b367a9f16870c7c53a65d1a36665534acd0cb5
michael5565 <michaelnwokobia60@gmail.com>
2022-07-06 07:02:37 -0700

    fgfds

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#inlude "main.h"
+/*
+ * multiolication table*/void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <=9; i++)
+	{
+		for (j = 0; j <= 0; j++)
+		{
+			_putchar(i * j);
+			_putchar(", ");
+		}
+	}
+	return (0)
+}
