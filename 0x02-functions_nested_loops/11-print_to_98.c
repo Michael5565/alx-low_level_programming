bc1a91150751022c0d4eaa12bc21e0d973d87a9b
michael5565 <michaelnwokobia60@gmail.com>
2022-07-05 20:43:38 -0700

    ddfd

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include"main.h"
+/*
+ * listing numbers*/void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d",n);
+			if (n != 98)
+			{
+				printf(", ");
+				n++;
+			}
+		}
+	}
+	else if (n > 98)
+	{
+
+		while (n >= 98)
+		{
+			printf("%d",n);
+				if (n != 98)
+				{
+					printf(", ");
+					n--;
+				}
+		}
+	}
+
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+
+}
