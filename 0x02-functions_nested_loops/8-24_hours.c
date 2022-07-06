a35ca50aee37b54509d9eb3c4ba35af0cb33b5e4
michael5565 <michaelnwokobia60@gmail.com>
2022-07-05 19:42:35 -0700

    sdd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,7 @@
  * time*/void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
@@ -11,8 +12,8 @@
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
-		        _putchar((m % 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
-}	
+}
