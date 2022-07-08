7d4898312a30eda7a7dd0160a176638a13e8816e
michael5565 <michaelnwokobia60@gmail.com>
2022-07-08 10:07:05 -0700

    hjhgg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,19 @@
 #include"main.h"
 /*
- * repsg*/void more_numbers(void)
+ * main*/void more_numbers(void)
 {
 	int n;
 	int a;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (a = '0'; a <= '14'; a++)
+		for (a = 0; a <= 14; a++)
 		{
 			if (a > 9)
 				_putchar((a / 10) + '0');
 			_putchar((a % 10) + '0');
 		}
-			_putchar('\n');
+	_putchar('\n');
 	}
 
 }
