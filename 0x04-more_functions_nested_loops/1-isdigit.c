c708c2a7148ee88bd290d46f424782ae931c2c89
michael5565 <michaelnwokobia60@gmail.com>
2022-07-07 19:11:38 -0700

    fddb

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,6 @@
 /**
  *main - check the code*/int _isdigit(int c)
 {
-
 	if (c >= 48 && c <= 57)
 	{
 		return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isupper(int c);
 int _isdigit(int c);
 void positive_or_negative(int i);
 int mul(int a, int b);
+void print_numbers(void);
