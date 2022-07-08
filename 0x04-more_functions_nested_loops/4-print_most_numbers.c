c0e7639ce37e927067931225a36db936b0b13442
michael5565 <michaelnwokobia60@gmail.com>
2022-07-07 19:30:23 -0700

    dsfdfd

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/*
+ * EXCEPTION*/void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num);
+			num++;
+		}
+	}
+	_putchar('\n');
+}
