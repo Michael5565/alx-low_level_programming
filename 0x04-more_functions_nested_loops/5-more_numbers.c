344749adb8d59a1d1a0cd356ddd9e5505650ab8b
michael5565 <michaelnwokobia60@gmail.com>
2022-07-08 09:34:36 -0700

    ggfhggh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,12 @@
 	for (n = 0; n < 10; n++)
 	{
 		for (a = '0'; a <= '14'; a++)
-			_putchar(a);
-		_putchar('\n');
+		{
+			if (a > 9)
+				_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+		}
+			_putchar('\n');
 	}
+
 }
