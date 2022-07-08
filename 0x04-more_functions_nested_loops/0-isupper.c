2ee28f583a8b3a64b2fa18bb66f7a64acebe5440
michael5565 <michaelnwokobia60@gmail.com>
2022-07-07 17:35:49 -0700

    eerf

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  *
- *  * main - check the code.
+ * main - check the code.
  *
- *   *
  *
- *    * Return: Always 0.
  *
- *     */
+ * Return: Always 0.
+ *
+ */
 
 int _isupper(int c)
 {
