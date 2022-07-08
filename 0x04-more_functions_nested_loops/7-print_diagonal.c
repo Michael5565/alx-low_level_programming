c620a38366f7221c165d6d26da4a524c06bd194d
michael5565 <michaelnwokobia60@gmail.com>
2022-07-08 11:31:09 -0700

    fghv

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,15 @@
-#include"main.h"
+#include "main.h"
 /*
  * diagonal*/void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 1; i <= n; i++)
 	{
-		_putchar('\\');
-		for (i = 2; i < n; i++)
-			{
-				_putchar('\n');
-				_putchar(' ');
-			}
+		for (j = 1; j < i; j++)
+			_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 	}
 }
